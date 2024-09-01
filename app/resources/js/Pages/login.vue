0926ee3c446fa4595186098fde59eee396fb8794
Qunexx <YtrikPlay@gmail.com>
2024-09-01 14:47:51 +0300

    Added components for login and register

--- app/resources/js/Pages/login.vue
+++ app/resources/js/Pages/login.vue
@@ -1,19 +1,68 @@
 <template>
     <DefaultLayout>
         <div class="container mt-5">
-            <h1 class="text-center">Авторизация</h1>
-            <button class="btn btn-primary">Нажми меня</button>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="mb-0">Вход</h4>
+                        </div>
+                        <div class="card-body">
+                            <div v-if="success" class="alert alert-success">
+                                {{ success }}
+                            </div>
+                            <div v-if="error" class="alert alert-danger">
+                                {{ error }}
+                            </div>
+                            <form @submit.prevent="submit">
+                                <div class="form-group mb-3">
+                                    <label for="email">Email</label>
+                                    <input v-model="email" type="email" class="form-control" id="email" placeholder="Введите email" required>
+                                    <div v-if="$page.props.errors.email" class="text-danger">{{ $page.props.errors.email }}</div>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="password">Пароль</label>
+                                    <input v-model="password" type="password" class="form-control" id="password" placeholder="Введите пароль" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block mb-3">Войти</button>
+                            </form>
+                            <div class="mt-3 text-center">
+                                <Link :href="route('register.index')">Нет аккаунта? Зарегистрируйтесь</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </DefaultLayout>
 </template>
 
 <script>
 import DefaultLayout from '../Shared/DefaultLayout.vue';
+import { Link } from "@inertiajs/inertia-vue3";
+import { route } from "ziggy-js";
 
 export default {
     components: {
+        Link,
         DefaultLayout
     },
-    props: {}
+    props: {
+        errors: Object,
+        error: String,
+        success: String,
+    },
+    data() {
+        return {
+            email: '',
+            password: '',
+        };
+    },
+    methods: {
+        route,
+        submit() {
+            this.$inertia.post('/login', { email: this.email, password: this.password });
+        },
+    },
 }
 </script>
--- app/resources/js/Pages/register.vue
+++ app/resources/js/Pages/register.vue
@@ -1,8 +1,40 @@
 <template>
     <DefaultLayout>
         <div class="container mt-5">
-            <h1 class="text-center">Регистрация</h1>
-            <button class="btn btn-primary">Нажми меня</button>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="mb-0">Регистрация</h4>
+                        </div>
+                        <div class="card-body">
+                            <form @submit.prevent="submit">
+                                <div class="form-group mb-3">
+                                    <label for="name">Имя</label>
+                                    <input v-model="name" type="text" class="form-control" id="name" placeholder="Введите имя" required>
+                                    <div v-if="errors.name" class="text-danger">{{ errors.name }}</div>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="email">Email</label>
+                                    <input v-model="email" type="email" class="form-control" id="email" placeholder="Введите email" required>
+                                    <div v-if="errors.email" class="text-danger">{{ errors.email }}</div>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="password">Пароль</label>
+                                    <input v-model="password" type="password" class="form-control" id="password" placeholder="Введите пароль" required>
+                                    <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="password_confirmation">Подтверждение пароля</label>
+                                    <input v-model="password_confirmation" type="password" class="form-control" id="password_confirmation" placeholder="Подтвердите пароль" required>
+                                    <div v-if="passwordMismatch" class="text-danger">Пароли не совпадают.</div>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block">Зарегистрироваться</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </DefaultLayout>
 </template>
@@ -14,6 +46,53 @@ export default {
     components: {
         DefaultLayout
     },
-    props: {}
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: '',
+            password_confirmation: '',
+            passwordMismatch: false,
+            errors: {},
+        };
+    },
+    methods: {
+        submit() {
+            this.errors = {};
+            if (!this.name) {
+                this.errors.name = 'Имя обязательно.';
+            }
+            if (!this.email) {
+                this.errors.email = 'Email обязателен.';
+            } else if (!this.validateEmail(this.email)) {
+                this.errors.email = 'Введите корректный email.';
+            }
+            if (!this.password) {
+                this.errors.password = 'Пароль обязателен.';
+            } else if (this.password.length < 8) {
+                this.errors.password = 'Пароль должен содержать минимум 8 символов.';
+            }
+            if (this.password !== this.password_confirmation) {
+                this.passwordMismatch = true;
+            } else {
+                this.passwordMismatch = false;
+            }
+
+            if (Object.keys(this.errors).length > 0 || this.passwordMismatch) {
+                return;
+            }
+
+            this.$inertia.post('/register', {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+                password_confirmation: this.password_confirmation,
+            });
+        },
+        validateEmail(email) {
+            const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return re.test(email);
+        },
+    },
 }
 </script>
