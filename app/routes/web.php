94f9baa0d605614e8fac8ddbdae715c42a7e77e0
Qunexx <YtrikPlay@gmail.com>
2024-09-01 14:48:52 +0300

    Added routes for login and register

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,31 +1,37 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Middleware\AuthMiddleware;
 
 Route::get('/', function () {
     return Inertia::render('main');
 })->name('home');
 
-Route::get('/register', function () {
-    return Inertia::render('register');
-})->name('register');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.index');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout.post');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout.post');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register.index');
+Route::post('/register', [RegisterController::class, 'register'])->name('register.post');
 
-Route::get('/login', function () {
-    return Inertia::render('login');
-})->name('login');
 
 Route::get('/about', function () {
     return Inertia::render('about');
 })->name('about');
 
+
 Route::get('/blog', function () {
     return Inertia::render('blog');
-})->name('blog');
+})->name('blog')->middleware(AuthMiddleware::class);
 
 Route::get('/contact', function () {
     return Inertia::render('contact');
 })->name('contact');
+
+
 
 
 
