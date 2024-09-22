4ab7eff7f6bd3794dd2a88933744905dafe94779
Qunexx <YtrikPlay@gmail.com>
2024-09-22 13:41:40 +0300

    Added notification's model and table

---
+++ app/app/Http/Controllers/NotificationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function markAsRead($id)
+    {
+
+    }
+}
