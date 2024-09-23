907b09b593857e0848faf085f05973f26fa6e550
Qunexx <YtrikPlay@gmail.com>
2024-09-23 15:12:40 +0300

    Fixed user's chat displaying, now user can see only chat's of user's chatting with. And moved logic from message controller to service

--- app/app/Http/Controllers/MessageController.php
+++ app/app/Http/Controllers/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\NewMessage;
 use App\Events\NewNotification;
+use App\Http\Services\MessagesService;
 use App\Http\Services\NotificationService;
 use App\Models\Message;
 use http\Env\Response;
@@ -15,51 +16,41 @@ use Inertia\Inertia;
 
 class MessageController extends Controller
 {
-    public function index() : \Inertia\Response
+    public function index(MessagesService $service) : \Inertia\Response
     {
-        $users = \App\Models\User::all();
+        $user = auth()->user();
+        $dialogUsers = $service->getDialogUsers($user);
         return Inertia::render('messages/index', [
             'initialMessages' => [],
-            'users' => $users,
+            'users' => $dialogUsers,
             'selectedRecipient' => null,
         ]);
     }
 
-    public function fetchMessages($recipientId) : \Inertia\Response
+    public function fetchMessages($recipientId, MessagesService $service) : \Inertia\Response
     {
-        $messages = \App\Models\Message::where('sender_id', auth()->id())
-            ->where('receiver_id', $recipientId)
-            ->orWhere('sender_id', $recipientId)
-            ->where('receiver_id', auth()->id())
-            ->with('sender')
-            ->get();
+        $user = auth()->user();
+        $messages = $service->getUserMessages($user, $recipientId);
+        $dialogUsers = $service->getDialogUsers($user);
 
         return Inertia::render('messages/index', [
             'initialMessages' => $messages,
-            'users' => \App\Models\User::all(),
+            'users' => $dialogUsers,
             'selectedRecipient' => (int)$recipientId,
         ]);
     }
 
-    public function store(Request $request) : RedirectResponse
+    public function store(Request $request,  MessagesService $service, NotificationService $notificationService) : RedirectResponse
     {
         $request->validate([
             'recipient_id' => 'required|exists:users,id',
             'message' => 'required|max:255',
         ]);
 
-        $message = \App\Models\Message::create([
-            'content' => $request->message,
-            'sender_id' => auth()->id(),
-            'receiver_id' => $request->recipient_id,
-        ]);
-
+        $message = $service->createMessage($request);
         $user = auth()->user();
         broadcast(new NewMessage($message, $request->recipient_id))->toOthers();
-        $notification = new NotificationService;
-        $notification->createNotification($request->recipient_id, "Новое сообщение от " . $user->name);
-//        broadcast(new NewNotification("New message"))->toOthers();
-
+        $notificationService->createNotification($request->recipient_id, "Новое сообщение от " . $user->name);
 
         return redirect()->route('messages.index', ['recipientId' => $request->recipient_id])->with('success', 'Сообщение отправлено!');
     }
---
+++ app/app/Http/Services/MessagesService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+
+class MessagesService
+{
+    public function getDialogUsers($user) : Collection //коллекций диалогов пользователя
+    {
+        $dialogUsers = Message::where('sender_id', $user->id)
+            ->orWhere('receiver_id', $user->id)
+            ->with('sender', 'receiver')
+            ->get()
+            ->pluck('sender')
+            ->merge(Message::where('sender_id', $user->id)
+                ->orWhere('receiver_id', $user->id)
+                ->with('sender', 'receiver')
+                ->get()
+                ->pluck('receiver'))
+            ->unique('id')
+            ->values();
+
+        return $dialogUsers;
+    }
+
+    public function getUserMessages($user, int $recipient_id) : Collection //коллекция сообщений с конкретным пользователем
+    {
+        $messages =  Message::where(function ($query) use ($user,$recipient_id) {
+            $query->where('sender_id', $user->id)
+                ->where('receiver_id', $recipient_id);
+        })->orWhere(function ($query) use ($user,$recipient_id) {
+            $query->where('sender_id', $recipient_id)
+                ->where('receiver_id', $user->id);
+        })->with('sender')->get();
+
+        return $messages;
+    }
+
+    public function createMessage(Request $request) : Message //создание сообщения
+    {
+        return Message::create([
+            'content' => $request->message,
+            'sender_id' => auth()->id(),
+            'receiver_id' => $request->recipient_id,
+        ]);
+    }
+
+}
