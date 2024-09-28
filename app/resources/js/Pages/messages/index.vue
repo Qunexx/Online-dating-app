7e573781e486b6844720d5ecc577b078784bc7b7
Qunexx <YtrikPlay@gmail.com>
2024-09-28 14:24:46 +0300

    Added smiles sending possibility to the chat

--- app/resources/js/Pages/messages/index.vue
+++ app/resources/js/Pages/messages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <DefaultLayout>
-        <div class="container mt-5">
+        <div class="container mt-5 mb-3">
             <div class="row">
                 <div class="col-md-4">
                     <div class="user-list card">
@@ -34,19 +34,28 @@
                             <div v-for="message in messages" :key="message.id" class="message">
                                 <div class="d-flex justify-content-between">
                                     <div>
-                                        <strong>{{ message.sender.name }}:</strong> {{ message.content }}
+                                        <strong>{{ message.sender.name || 'Загрузка...' }}:</strong> {{ message.content }}
                                     </div>
                                     <div class="message-date">
-                                        {{ formatDate(message.created_at) }}
+                                        {{ formatDate(message.created_at) || 'Загрузка...' }}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <form @submit.prevent="sendMessage" class="mt-3">
+                    <form v-if="selectedRecipient" @submit.prevent="sendMessage" class="mt-3">
                         <div class="input-group">
-                            <input v-model="newMessage" placeholder="Type a message" class="form-control" />
-                            <button type="submit" class="btn btn-primary">Send</button>
+                            <button type="button" @click="toggleEmojiPicker" class="btn btn-secondary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                    <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
+                                </svg>
+                            </button>
+                            <input v-model="newMessage" placeholder="Напишите сообщение" class="form-control" />
+                            <button type="submit" class="btn btn-primary">Отправить</button>
+                        </div>
+                        <div v-if="showEmojiPicker" class="emoji-picker-container">
+                            <emoji-picker @emoji-click="onEmojiClick"></emoji-picker>
                         </div>
                     </form>
                 </div>
@@ -61,6 +70,7 @@ import Echo from 'laravel-echo';
 import DefaultLayout from "../../Shared/DefaultLayout.vue";
 import axios from 'axios';
 import {route} from "ziggy-js";
+import 'emoji-picker-element';
 
 
 export default {
@@ -78,6 +88,7 @@ export default {
             scrollPosition: 0,
             isAtBottom: true,
             selectedRecipientName: '',
+            showEmojiPicker: false,
         };
     },
     mounted() {
@@ -162,6 +173,12 @@ export default {
                 this.isAtBottom = this.$refs.chatBox.scrollTop + this.$refs.chatBox.clientHeight >= this.$refs.chatBox.scrollHeight;
             }
         },
+        onEmojiClick(event) {
+            this.newMessage += event.detail.unicode;
+        },
+        toggleEmojiPicker() {
+            this.showEmojiPicker = !this.showEmojiPicker;
+        },
         async sendMessage() {
             try {
                 const response = await axios.post('/messages', {
@@ -176,7 +193,9 @@ export default {
             }
         },
         formatDate(date) {
+            if (!date) return '';
             const dateObj = new Date(date);
+            if (isNaN(dateObj.getTime())) return '';
             const timeOptions = { hour: '2-digit', minute: '2-digit' };
             const dateOptions = { day: '2-digit', month: '2-digit', year: '2-digit' };
             const timeString = dateObj.toLocaleTimeString('ru-RU', timeOptions);
@@ -200,5 +219,12 @@ export default {
 
 .message {
     margin-bottom: 10px;
+}
+
+.emoji-picker-container {
+    position: absolute;
+    left: 40%;
+    transform: translateX(-50%);
+    z-index: 1000;
 }
 </style>
